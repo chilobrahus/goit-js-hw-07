9108b30c3e8dc437fef1da598d9507c05496b075
chilobrahus <slava.z.monte@gmail.com>
2022-12-26 18:38:24 +0200

    hw 7

---
+++ css/js/02-lightbox.js
@@ -0,0 +1,15 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+    .map((item) =>
+        `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>`)
+    .join("");
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', { close: false, captionDelay: 250, captionsData: 'alt' });
